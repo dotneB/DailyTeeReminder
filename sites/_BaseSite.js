1b1c5dcaeef15d9d0d86514dd1be8e488af913a2
dotneB <benoit@tioneb.ca>
2012-06-09 20:27:01 -0400

    Option page with a way to disable/enable sites

--- sites/_BaseSite.js
+++ sites/_BaseSite.js
@@ -16,6 +16,13 @@ BaseSite.prototype.setTeeImage     = function(value)    { localStorage[this.site
 BaseSite.prototype.getTeeImage     = function()         { return localStorage[this.siteName + "_teeImgSrc"]; }
 BaseSite.prototype.setRead         = function(value)    { localStorage[this.siteName + "_read"] = value; }
 BaseSite.prototype.isRead          = function()         { return localStorage[this.siteName + "_read"] == "true"; }
+BaseSite.prototype.setEnabled      = function(value)    { localStorage[this.siteName + "_enabled"] = value; }
+BaseSite.prototype.getEnabled      = function(value)    { return localStorage[this.siteName + "_enabled"]; }
+BaseSite.prototype.isEnabled       = function()
+{
+    if(this.getEnabled() == undefined || this.getEnabled() == "undefined") this.setEnabled(true);
+    return this.getEnabled() == "true"; 
+}
 
 BaseSite.prototype.setContent      = function(teeTitle, teeImage)
 {
@@ -29,16 +36,19 @@ BaseSite.prototype.setContent      = function(teeTitle, teeImage)
 
 BaseSite.prototype.writeSlide      = function(container)
 {
-    $(container).append(
-        "<div class=\"slide\">" +
-            "<div class=\"slide_content\">" +
-                ( this.isRead() ? "" : "<img src=\"assets/new-ribbon.png\" width=\"112\" height=\"112\" alt=\"New Ribbon\" id=\"ribbon\">") +
-                "<a href=\"#\" onclick=\"chrome.tabs.create({url: '" + this.siteURL + "'})\"><img src=\"" + this.getTeeImage() + "\" width=\"" + this.imgWidth + "\" height=\"" + this.imgHeight + "\"></a>" +                
-            "</div>" +
-            "<div class=\"caption\" style=\"bottom:0\">" +
-                "<p>" + this.siteDisplayName + ": " + this.getTeeTitle() +"</p>" +
-            "</div>" +
-        "</div>"
-    );
-    this.setRead(true);
+    if(this.isEnabled())
+    {
+        $(container).append(
+            "<div class=\"slide\">" +
+                "<div class=\"slide_content\">" +
+                    ( this.isRead() ? "" : "<img src=\"assets/new-ribbon.png\" width=\"112\" height=\"112\" alt=\"New Ribbon\" id=\"ribbon\">") +
+                    "<a href=\"#\" onclick=\"chrome.tabs.create({url: '" + this.siteURL + "'})\"><img src=\"" + this.getTeeImage() + "\" width=\"" + this.imgWidth + "\" height=\"" + this.imgHeight + "\"></a>" +                
+                "</div>" +
+                "<div class=\"caption\" style=\"bottom:0\">" +
+                    "<p>" + this.siteDisplayName + ": " + this.getTeeTitle() +"</p>" +
+                "</div>" +
+            "</div>"
+        );
+        this.setRead(true);
+    }
 }
